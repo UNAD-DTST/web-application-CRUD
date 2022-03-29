f75fe8685b3181fb15dd002e79a9c473cf251816
Felipe Garzón Castro <afgarzonc@gmail.com>
2022-03-28 21:00:11 -0500

    Create functionality

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+// Connection to DB
+$conection = mysqli_connect(
+    'localhost',
+    'root',
+    'secret',
+    'web_application_crud'
+
+);
---
+++ save_employee.php
@@ -0,0 +1,41 @@
+<?php
+// Include db connection file
+include("db.php");
+
+//Check if POST method has parameter to create employee
+if (isset($_POST["create_employee"])) {
+    $email = $_POST['input-email'];
+    $identification = $_POST['input-identification'];
+    $name = $_POST['input-name'];
+    $surname = $_POST['input-surname'];
+    $title = $_POST['input-title'];
+    $address = $_POST['input-address'];
+    $phone = $_POST['input-phone'];
+
+    // $photo = $_POST['input-photo'];
+
+    if (!empty($_FILES["input-photo"]["name"])) {
+        // Get file info 
+        $fileName = basename($_FILES["input-photo"]["name"]);
+        $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
+
+        // Allow certain file formats 
+        $allowTypes = array('jpg', 'png', 'jpeg');
+        if (in_array($fileType, $allowTypes)) {
+            $image = $_FILES['input-photo']['tmp_name'];
+            $imgContent = addslashes($_FILES["input-photo"]["name"]);
+        }
+    }
+
+    // Query to upload employee info into DB
+    $query = "INSERT INTO employees (email, identification, name, surname, title, address, phone, photo)
+    VALUES ('$email', '$identification', '$name', '$surname', '$title', '$address', '$phone', '{$imgContent}')";
+    $result = mysqli_query($conection, $query);
+    var_dump($result);
+    if (!$result) {
+        die("Query Failed");
+    }
+
+    //Redirect
+    header("Location: employees.php");
+}
